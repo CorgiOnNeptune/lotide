7eb83d71b568a3341a1907cf4646c6b687f60aba
Justin <justinjdbennett@gmail.com>
2022-11-14 23:55:46 +0000

    assertion fucntion

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// Function implementation
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test code
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(2 + 5, 2);
+assertEqual(0, null);
