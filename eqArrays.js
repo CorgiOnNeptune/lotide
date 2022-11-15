906f4f1669b4eda0db791e448f5c0d8bf742a1b8
Justin <justinjdbennett@gmail.com>
2022-11-15 23:15:53 +0000

    Add function to check if arrays are strictly equivalent

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// Assertion function
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
+  } else {
+    return console.log(`❌ Assertion Failed: ${actual} !== ${expected} ❌`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  let storedValue = 0;
+  
+  for (let key in arr1) {
+    if (arr1[key] === arr2[key])
+      storedValue++;
+  }
+  if (storedValue % arr1.length === 0) {
+    return true;
+  }
+  return false;
+};
+
+// Test results
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
