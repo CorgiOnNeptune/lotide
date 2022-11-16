a4733fa20dccfd4fe8da60503fbddab0897c8bcf
Justin <justinjdbennett@gmail.com>
2022-11-16 22:41:46 +0000

    implement countLetters function

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+// Function implementation
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
+  } else {
+    return console.log(`❌ Assertion Failed: ${actual} !== ${expected} ❌`);
+  }
+};
+
+const countLetter = (string) => {
+  const charactersObject = {};
+  string = string.replace(/[^a-z0-9]/gi, '');
+
+  for (const char of string) {
+      if (charactersObject[char]) {
+        charactersObject[char] += 1;
+      } else {
+        charactersObject[char] = 1;
+      }
+  } 
+    
+  return charactersObject;
+};
+
+console.log(countLetter('LHL'));
+console.log(countLetter('Lighthouse Labs'))
+console.log(countLetter('Diamonds on Neptune!!!!!!'))
+
+
+// assertEqual(countLetter('LHL'));
+// assertEqual(countLetter('Gfdauhire3qauA'));
+// assertEqual(countLetter('G'));
+// assertEqual(countLetter('G'));
