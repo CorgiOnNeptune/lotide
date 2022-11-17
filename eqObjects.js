a54c4f41230a7e016d0ea431577585b86efbe1d8
Justin <justinjdbennett@gmail.com>
2022-11-17 00:34:47 +0000

    implement eqObjects function to compare equivalency of two given objects and their keys/values

---
+++ eqObjects.js
@@ -0,0 +1,67 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
+  } else {
+    return console.log(`❌ Assertion Failed: ${actual} !== ${expected} ❌`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let key in arr1) {
+    if (arr1[key] !== arr2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function (object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+
+  // Check if objects have the same amount of keys
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+
+  // Check if the values of the keys on each object match.
+  for (const key of object1Keys) {
+
+    // Check if value of the key is an array.
+    // If so, use eqArrays function to compare
+    // the values then return the result.
+    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+      return eqArrays(object1[key], object2[key]);
+    }
+    
+    // If value is not an array, check if they match.
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+// Test code
+const ab = { a: '1', b: '2' };
+const ba = { b: '2', a: '1' };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: '1', b: '2', c: '3' };
+assertEqual(eqObjects(ab, abc), false);
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
