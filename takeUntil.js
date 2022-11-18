9d04449a9115bb7b09bf8f0ba40887d0e72bba75
Justin <justinjdbennett@gmail.com>
2022-11-18 01:05:14 +0000

    Add takeUntil function to lotide

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+// Arrays assertion function
+const eqArrays = (arr1, arr2) => {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return false;
+  }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let key in arr1) {
+    if (arr1[key] !== arr2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected} ✅`);
+  }
+
+  return console.log(`❌ Assertion Failed: ${actual} !== ${expected} ❌`);
+};
+
+
+// This function will return a "slice of the array with elements taken from the beginning."
+// It should keep going until the callback returns a truthy value.
+
+const takeUntil = (array, callback) => {
+  let results = [];
+
+  for (const element of array) {
+    if (!callback(element))
+      results.push(element);
+
+    if (callback(element))
+      return results;
+  }
+};
+
+// test code
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
