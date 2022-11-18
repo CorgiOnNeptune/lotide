2b8b8a024d1a908f75ee5e9f499de2a69e3d779b
Justin <justinjdbennett@gmail.com>
2022-11-18 22:52:23 +0000

    Refactor countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,18 +1,20 @@
-const countLetter = (string) => {
-  const charactersObject = {};
-  string = string.replace(/[^a-z0-9]/gi, '');
+const countLetter = (str) => {
+  const result = {};
+  // Remove non-alpha-numeric characters from string
+  str = str.replace(/[^a-z0-9]/gi, '');
 
-  for (const char of string) {
-    if (charactersObject[char]) {
-      charactersObject[char] += 1;
-    } else {
-      charactersObject[char] = 1;
+  for (const char of str) {
+    if (result[char] === undefined) {
+      result[char] = 0;
     }
+
+    result[char]++;
   }
     
-  return charactersObject;
+  return result;
 };
 
 console.log(countLetter('LHL'));
 console.log(countLetter('Lighthouse Labs'));
 console.log(countLetter('Diamonds on Neptune!!!!!!'));
+console.log(countLetter('L1ghth0u53 L4b5'));
