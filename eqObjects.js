ddb13808a812dd9c9c47fdab7d73bb0ce1ac3e78
Justin <justinjdbennett@gmail.com>
2022-11-19 20:50:50 +0000

    Recursive eqObjects! Thanks to Gary.

--- eqObjects.js
+++ eqObjects.js
@@ -21,39 +21,45 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-const eqObjects = function(object1, object2) {
-  const object1Keys = Object.keys(object1);
-  const object2Keys = Object.keys(object2);
+
+const eqObjects = (object1, object2) => {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
   // Check if objects have the same amount of keys
-  if (object1Keys.length !== object2Keys.length) {
+  if (keys1.length !== keys2.length) {
     return false;
   }
 
-  // Check if the values of the keys on each object match.
-  for (const key of object1Keys) {
+  const isObject = (val) => {
+    return typeof val === 'object' && !Array.isArray(val);
+  };
 
-    // Check if value of the key is an array.
-    // If so, use eqArrays function to compare
-    // the values then return the result.
-    if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
-      return eqArrays(object1[key], object2[key]);
+  for (const key of keys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    // Check if values are arrays. If they are, eqArrays.
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+      continue;
     }
-    
-    // If value is not an array, check if they match.
-    if (object1[key] !== object2[key]) {
+    // Check if values are objects, if they are, recurse.
+    if (isObject(value1) && isObject(value2)) {
+      if (!eqObjects(value1, value2)) {
+        return false;
+      }
+      continue;
+    }
+
+    if (value1 !== value2) {
       return false;
     }
   }
-
   return true;
 };
-
-// const results1 = { a: { z: 1 }, b: 2 };
-// const results2 = { a: { y: 0, z: 1 }, b: 2 };
-
-// console.log(Object.keys(results1).length);
-// console.log(results2.length);
 
 
 // Recursion test code
@@ -66,19 +72,18 @@ console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }));
 console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }));
 // => false
 
-
 // Test code
-// const ab = { a: '1', b: '2' };
-// const ba = { b: '2', a: '1' };
-// assertEqual(eqObjects(ab, ba), true);
+const ab = { a: '1', b: '2' };
+const ba = { b: '2', a: '1' };
+assertEqual(eqObjects(ab, ba), true);
 
-// const abc = { a: '1', b: '2', c: '3' };
-// assertEqual(eqObjects(ab, abc), false);
+const abc = { a: '1', b: '2', c: '3' };
+assertEqual(eqObjects(ab, abc), false);
 
 
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
 
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false);
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
