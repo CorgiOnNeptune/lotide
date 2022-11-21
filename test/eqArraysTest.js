fc6cc93ee202684a456a2a97b77b96a26a228197
Justin <justinjdbennett@gmail.com>
2022-11-21 22:11:06 +0000

    refactor eqArrays using mocha/chai test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,16 +1,50 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../index').eqArrays;
 
-// recursion test code
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-assertEqual(eqArrays([[[[[[[[2], 3], [4]]]]]]], [[[[[[[[2], 3], [4]]]]]]]), true); // => true
-assertEqual(eqArrays([3, [4]], [3, [4]]), true);
+describe('#eqArrays', () => {
+  it('should return true, given two arrays with equal nubmer values', () => {
+    const arr1 = [1, 2, 3];
+    const arr2 = [1, 2, 3];
+    assert.strictEqual(eqArrays(arr1, arr2), true);
+  });
 
-// Test code
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  it('should return false, given two arrays of different lengths', () => {
+    const arr1 = [1, 2, 3];
+    const arr2 = [3, 2, 1];
+    assert.strictEqual(eqArrays(arr1, arr2), false);
+  });
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  it('should return true, given two arrays with equal string values', () => {
+    const arr1 = ["1", "2", "3"];
+    const arr2 = ["1", "2", "3"];
+    assert.strictEqual(eqArrays(arr1, arr2), true);
+  });
+
+  it('should return false, given two arrays of similar values with inequal types', () => {
+    const arr1 = ["1", "2", "3"];
+    const arr2 = ["1", "2", 3];
+    assert.strictEqual(eqArrays(arr1, arr2), false);
+  });
+
+  it('should return true, given equal primitve values', () => {
+    assert.strictEqual(eqArrays('1', '1'), true);
+  });
+
+  it('should return true, given two equal nested arrays', () => {
+    const arr1 = [[2, 3], [4]];
+    const arr2 = [[2, 3], [4]];
+    assert.strictEqual(eqArrays(arr1, arr2), true);
+  });
+
+  it('should return false, given two inequal nested arrays', () => {
+    const arr1 = [[2, 3], [4]];
+    const arr2 = [[2, 3], [4, 5]];
+    assert.strictEqual(eqArrays(arr1, arr2), false);
+  });
+
+  it('should return true, given deeper nested arrays', () => {
+    const arr1 = [[[[[[[[2], 3], [4]]]]]]];
+    const arr2 = [[[[[[[[2], 3], [4]]]]]]];
+    assert.strictEqual(eqArrays(arr1, arr2), true);
+  });
+});
