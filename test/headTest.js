bcc4cbbfb397054aa585542d59a1665b17eb8f31
Justin <justinjdbennett@gmail.com>
2022-11-21 06:44:58 +0000

    Missed adding headTest mocha/chai refactor

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
-assertEqual(head([]),);
-assertEqual(head(['one']), 1);
-assertEqual(head([1, 2, 3, 4, 5, 10]), 10);
+describe('#head', () => {
+  it('returns 1 for [1,2,3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('returns \'5\' for [\'5\']', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns false when comparing '1' as a string vs 1 as number.", () => {
+    assert.notStrictEqual(head(['1']), 1);
+  });
+});
